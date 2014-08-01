540a8503453fb636aab172fea950592ebab62f9e
Hootie81 <chris.huitema@iinet.net.au>
2014-08-02 02:07:36 +0800

    Added License

--- Dual DC Motor v1-0/Library/SPI_Motor/inc/SPI_Motor.h
+++ Dual DC Motor v1-0/Library/SPI_Motor/inc/SPI_Motor.h
@@ -1,4 +1,4 @@
-/******************************************************************************* 
+/****************************************************************************** 
   This is a library for Hootie81's Dual DC Motor Shield for Spark Core
 
  The Dual DC Motor Shield uses a Maxim MAX6966 SPI PWM I/O expander 
@@ -26,11 +26,23 @@
  be determined, but will probably be limited by RAM on the core or Lag in motor
  control from the need to clock data through multiple cards.
  
- 
- 
-  Written by Chris Huitema. 30/7/14 
-  
- ******************************************************************************/
+********************************************************************************
+
+Copyright 2014  Chris Huitema.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*****************************************************************************/
 
 #include "application.h"
 
@@ -122,7 +134,3 @@ private:
     boolean checkTotalCards(uint8_t);
 
 };
-
-
-
-
--- Dual DC Motor v1-0/Library/SPI_Motor/src/SPI_Motor.cpp
+++ Dual DC Motor v1-0/Library/SPI_Motor/src/SPI_Motor.cpp
@@ -26,11 +26,23 @@
  be determined, but will probably be limited by RAM on the core or Lag in motor
  control from the need to clock data through multiple cards.
  
- 
- 
-  Written by Chris Huitema. 30/7/14 
-  
- *****************************************************************************/
+********************************************************************************
+
+Copyright 2014  Chris Huitema.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*****************************************************************************/
 
 #include "SPI_Motor.h"
 
